ad83fbb560b1669b7270f9cf20e3bb799e153839
DeniseHeid <40720106+DeniseHeid@users.noreply.github.com>
2018-07-06 06:28:22 -0400

    Add files via upload

---
+++ pricing.php
@@ -0,0 +1,196 @@
+<?php
+
+// Create data list for knitting application
+
+$knitting = array(
+
+array('name' => 'MOHR_FOR_HIM', 'gender' => 'male', 'type' => 'sweater', 'small' => '$159.00', 'large' => '$265.00', 'skeins_small' => '12', 'skeins_large' => '20', 'grams' => '50', 'skein_price' =>'$13.25',	'yarn' =>'Shelter', 'yarn_brand'=> 'Brooklyn Tweed', 'image'=> 'Mohr_his_NG.png'),
+array('name' =>'DENALI', 'gender' =>'male', 'type' => 'sweater', 'small' => '$159.00', 'large' => '$251.75', 'skeins_small' => '12', 'skeins_large' => '19', 'grams' => '50','skein_price' => '$13.25', 'yarn' =>'Shelter', 'yarn_brand' => 'Brooklyn Tweed', 'image'=>'Denali_his_NG.png'),
+array('name' => 'NORTH', 'gender' =>'male', 'type' => 'sweater', 'small' => '$59.50', 'large' => '$110.50', 'skeins_small' => '7',	'skeins_large' =>'13', 'grams' => '100', 'skein_price'	=>'$8.50', 'yarn' => 'Vintage Chunky', 'yarn_brand' => 'Berroco', 'image'=> 'North_mens_AmyChristoffers.png'),
+array('name' =>	'KETCH', 'gender' =>'male', 'type' => 'sweater', 'small' => '$51.00', 'large' => '$93.50', 'skeins_small' => '6',	'skeins_large' =>'11',	'grams' => '100', 'skein_price'	=>'$8.50', 'yarn' => 'Vintage',	'yarn_brand' => 'Berroco', 'image' => 'Ketch_mens_AmyChristoffers.png'),
+array('name' =>	'Burkina','gender' =>'female',	'type' => 'sweater',	 'small' => '$26.00', 'large' => '$39.00', 'skeins_small' => '4', 'skeins_large' =>'6',	'grams' => '100', 'skein_price'	=>'$6.50', 	'yarn' => 'Weekend', 'yarn_brand' => 'Berroco', 'image'=> 'Burkina_NG.png'),
+array('name' =>	'Parkview',	'gender' =>'female', 'type' => 'sweater', 'small' =>	 '$120.00','large' => '$210.00', 'skeins_small' => '8',	'skeins_large' =>'14',	'grams' => '50', 'skein_price'	=>'$15.00','yarn' => 'Knightsbridge', 'yarn_brand' => 'The Fibre Co.', 'image'=> 'ParkView_NG.png'),
+array('name' =>	'Metropolis', 'gender' =>'female',	'type' => 'sweater', 'small' => '$108.00', 'large' => '$198.00', 'skeins_small' => '6', 'skeins_large' =>'11',	'grams' => '50', 'skein_price'	=>'$18.00', 'yarn' => 'Road to China Light',	'yarn_brand' => 'The Fibre Co.', 'image'=> 'Metropolis_NG.png'),
+array('name' =>	'Mohr_For_Her', 'gender' =>'female', 'type' => 'sweater', 'small' => '$145.75', 'large' => '$212.00', 'skeins_small' => '11', 'skeins_large' =>'16',	'grams' => '50', 'skein_price'	=>'$13.25', 'yarn' => 'Shelter', 'yarn_brand' => 'Brooklyn Tweed', 'image'=> 'Mohr_hers_NG.png'),
+array('name' =>	'DINE Cardigan', 'gender' =>'female', 'type' => 'sweater', 'small' => '$96.00', 'large' => '$156.00', 'skeins_small' => '8',	'skeins_large' =>'13',	'grams' => '100', 'skein_price'	=>'$12.00', 'yarn' => 'Alpaca Chunky', 'yarn_brand' => 'Berroco', 'image'=> 'Dine_Cardigan_MicheleWang.png'),
+array('name' =>	'Cookie', 'gender' =>'female',	'type' => 'sweater', 'small' => '$148.50', 'large' => '$216.00', 'skeins_small' => '11',	'skeins_large' =>'16',	'grams' => '50', 'skein_price'	=>'$13.50', 'yarn' => 'Brushed Fleece',	'yarn_brand' => 'Rowan', 'image'=> 'Cookie_SarahHatton.png'),
+array('name' =>	'Hoehne Vest', 'gender' =>'female',	'type' => 'sweater',	 'small' => '$36.00', 'large' => '$72.00', 'skeins_small' => '3', 'skeins_large' =>'6',	'grams' => '100', 'skein_price'	=>'$12.00', 'yarn' => 'Ultra Alpaca', 'yarn_brand' => 'Berroco', 'image'=> 'Hoehne_Vest_CourtneySpainhower.png'),
+array('name' =>	'Woodbine',	'gender' =>'female', 'type' => 'sweater', 'small' => '$99.00', 'large' => '$176.00',	'skeins_small' => '9', 'skeins_large' =>'16', 'grams' => '50','skein_price'	=>'$11.00', 'yarn' => 'Tuscan Tweed', 'yarn_brand' => 'Berroco', 'image'=> 'Woodbine_AmyChristoffers.png'),
+array('name' =>	'Harebell',	'gender' =>'female', 'type' => 'sweater', 'small' => '$88.00', 'large' => '$143.00', 'skeins_small' => '8', 'skeins_large' =>'13', 'grams' => '50', 'skein_price'=>'$11.00', 'yarn' => 'Tuscan Tweed', 'yarn_brand' => 'Berroco', 'image'=> 'Harebell_AmyChristoffers.png'),
+array('name' =>	'Bergere de France-scarf',	'gender' =>'male', 'type' => 'scarves_hats', 'small' =>'$57.00','large' => '$57.00', 	 'skeins_small' => '6',	'skeins_large' => '6',	'grams' => '50', 'skein_price'=>'$9.50', 'yarn' => 	'Magic Avalanche', 'yarn_brand' => 'Bergere de France','image'=>'BergeredeFrance_scarf.png'),
+array('name' =>	'Snare', 'gender' =>'male',	'type' => 'scarves_hats', 'small' => '$13.25','large' => '$13.25', 'skeins_small' => '1', 'skeins_large' =>'1',	'grams' => '50', 'skein_price'	=>'$13.25', 'yarn' => 'Shelter', 'yarn_brand' => 'Brooklyn Tweed', 'image'=> 'Snare_hat_his_NG.png'),
+array('name' =>	'JIB',	'gender' =>'male', 'type' => 'scarves_hats', 'small' => '$8.50','large' => '$8.50', 'skeins_small' => '1', 'skeins_large' =>'1', 'grams' => '100',	'skein_price' =>'$8.50', 'yarn' =>	'Vintage',	'yarn_brand' => 'Berroco', 'image'=> 'JIB_mens_AlisonGreen.png'),
+array('name' =>	'Priyome', 'gender' =>'female',	'type' => 'scarves_hats','small' => '$17.50','large' => '$17.50', 'skeins_small' => '1', 'skeins_large' =>'1',	'grams' => '100', 'skein_price'	=> '$17.50', 'yarn' =>	'Noble', 'yarn_brand' => 'Berroco', 'image'=> 'Priyome_Emily.jpg'),    
+array('name' =>	'Lake Volta', 'gender' =>'female',	'type' => 'scarves_hats', 'small' => '$13.47', 'large' => '$13.47', 'skeins_small' => '3', 'skeins_large' =>'3', 'grams' => '50', 'skein_price'	=> '$4.49', 'yarn' => 'Linus',	'yarn_brand' => 'Berroco', 'image'=> 'Lake_Volta_NG.png'),           
+array('name' =>	'Intrepid',	'gender' =>'female', 'type' => 'scarves_hats', 'small' => '$14.98', 'large' => '$14.98','skeins_small' => '2', 'skeins_large' =>'2', 'grams' => '50', 'skein_price'	=>'$7.49', 	'yarn' => 'Canopy Fingering', 'yarn_brand' => 'The Fibre Co.', 'image'=> 'Intrepid_NG.png'),
+array('name' =>	'Teeter Totter Shawl', 'gender' =>'female',	'type' => 'scarves_hats', 'small' => '$65.00', 'large' => '$65.00',  'skeins_small' => '5',	'skeins_large' =>'5','grams' => '100', 'skein_price' => '$13.00', 'yarn' =>	'Ultra Alpaca Fine', 'yarn_brand' => 'Berroco', 'image'=> 'TeeterTotter_Shawl_JuliaFarwell-Clay.png'),
+array('name' =>	'Hana',	'gender' =>'female', 'type' => 'scarves_hats','small' => '$40.00', 'large' => '$40.00','skeins_small' => '2', 'skeins_large' =>'2',	'grams' => '250', 'skein_price'	=>'$20.00', 'yarn' =>'Macro', 'yarn_brand' => 'Berroco', 'image'=> 'Hana_EmilyNoraONeil.png')
+);
+
+
+// Define Variables for Form Submission
+$genders = array('male'=>'Male', 'female'=>'Female'); 
+$types = array('sweater'=>'Sweater', 'scarves_hats'=>'Scarves_Hats'); 
+$results = array();
+$errors = array();	 
+
+	 
+// Run matching algorithm
+	
+	$results = array();
+	
+		if(isset($_POST['gender']) && isset($_POST['type'])) {  // The form has been submitted
+			
+			if(!array_key_exists($_POST['gender'], $genders))
+					$errors['gender'] = 'Please select a gender option';
+		 
+			if(!array_key_exists($_POST['type'], $types))
+					$errors['type'] = 'Please select a type option';	
+
+			if(!$errors){	
+
+			foreach($knitting as $key=>$value){			
+							
+				if($value['gender'] == $_POST['gender'] && $value['type'] == $_POST['type'])
+					$results[] = $value;
+				}					
+				
+			}
+		}
+			
+		 
+	
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Knitting Pattern Pricing</title>
+		<style>
+
+			img {height: 200px; width: 200px}  
+			p::first-letter {font-size:200%; color: purple}  
+			body {background-color: rgb(190,226,248)}
+			h1 {font-style: italic} 
+			
+			h1 {font-size: 150%}
+			h3{font-size: 115%}
+			p {font-size: 125%}
+			
+			legend {font-size: 125%}
+			label {font-size: 125%}
+
+			div#learned {text-align: left}
+
+			ul#contact {list-style-type: none}
+
+			ul#horizontal {border-radius: 10px; border-bottom: 4px solid black; border-top: 4px solid black}
+			ul#horizontal {padding: 0}
+			ul#horizontal {background-color: rgb(223,220,222); list-style-type: none; line-height: 50px}
+			ul#horizontal ul li {text-align: center; font-size: bold, font-size: 50%}
+
+			ul#vertical {list-style-type: none}
+			ul{list-style-type: none}
+			
+			div#contact_info {text-align: center}
+			footer {text-align: center}
+
+		</style>
+		
+		
+		
+	</head>
+	
+	<body>
+
+	<h1>Knitting Pattern Pricing</h1>
+	
+				<p>  <img src="img/pricing.jpg" alt="pricing" title="pricing the knitting projects"> 
+				<img src="img/pleasures.jpg" alt="pleasures" title="tea and cookies"> </p>
+								
+				<h3>Pricing Method Used: </h3>
+				<h3> 1. based on original pattern yarn brands</h3>
+				<h3> 2. based on actual prices researched on the dates of 1/20/2017 and 1/21/2017 </h3>  				
+				 				
+				<p>Please select the options for gender and type, then press the submit key.</p>			
+	
+	<form action="pricing.php" method="POST">
+	<form id="myForm">
+
+		<fieldset>
+		<legend> Select by Gender: Men or Women</legend> 
+			<label for="gender">Gender</label>
+				<select id="gender" name="gender" >
+				<option value="choose">Choose Gender</option>
+				<option value ="male"> Men's </option>
+				<option value = "female"> Women's </option>
+				</select><br>
+				<p> <?php if(isset($errors['gender'])) {
+							echo 'Please select a gender option';
+							}?> </p>
+				<p id="gender_error"> </p>
+		
+		</fieldset>
+			
+		<fieldset>
+			<legend> Select by Type: Sweaters or Scarves/ Hats </legend> 
+			<label for="type">Type</label>
+			<select id="type" name="type" >
+				<option value="choose"> Choose Type</option>
+				<option value="sweater">Sweaters</option>
+				<option value="scarves_hats">Scarves/ Hats</option>
+				</select><br>
+				<p> <?php if(isset($errors['type'])) {
+							echo 'Please select a type option';
+							}?> </p>
+				<p id="type_error"> </p>
+				
+		</fieldset>
+		
+		<fieldset>			
+			<input id="submit" type="submit" value="submit" />
+		</fieldset>
+
+						
+	</form> 
+	
+	<?php			
+	
+		if($results){
+					
+			echo '<ul>';
+			foreach($results as $key => $value){ 
+				echo '<li>';
+				
+				echo '<p> GENDER: '.$value['gender'].' & TYPE: '.$value['type'].' </p>';
+				echo '<p> NAME: '.$value['name'].' </p>';
+				echo '<p> YARN NAME: '.$value['yarn'].' & YARN BRAND: '.$value['yarn_brand'].' </p>';
+				echo '<p> <img src="img/'.$value['image'].'"/> </p>' ;
+				
+				echo '<p> PRICING </p>';
+				echo '<h3> Small Size: '.$value['small'].' </h3>';
+				echo '<h3> Large Size: '.$value['large'].' </h3>';
+				echo '<h3> Skein: '.$value['skein_price'].' </h3>';
+								
+				echo '<p> SKEINS NEEDED </p>';
+				echo '<h3> Small size: '.$value['skeins_small'].' </h3>';
+				echo '<h3> Large size: '.$value['skeins_large'].' </h3>';
+				echo '<h3> Grams per skein: '.$value['grams'].' </h3> <br>';
+				
+				echo '</li>';
+				
+			}
+			echo '</ul>';
+			
+			}
+				
+				
+				
+
+	?>
+	
+	</body>
+	
+</html>
